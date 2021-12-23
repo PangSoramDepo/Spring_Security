71df8665dc19d5a93b97f2be5a2be3a300bfe207
PANGSORAMDEPO <pangsoramdepo@gmail.com>
2021-12-23 13:33:40 +0700

    Feat: Config Web Security With Default UserPassword

---
+++ rbac/src/main/java/com/pangsoramdepo/rbac/service/UserDetailServiceImp.java
@@ -0,0 +1,18 @@
+package com.pangsoramdepo.rbac.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserDetailServiceImp implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("depo", "depo1234", new ArrayList<>());
+    }
+}
